22a2f6c234a6491380cd1cfd2277d22a516c3be8
Ankit Gupta <ag3831124@gmail.com>
2020-08-07 15:58:08 +0530

    some changes

--- image_optimizer.py
+++ image_optimizer.py
@@ -5,25 +5,26 @@ from PIL import Image
 
 def functionality():
     print('Starting')
+  
+for folders in range(1, len(sys.argv)):
+   #  print(len(sys.argv))
     try :
-       images_folder = sys.argv[1]
-    
+       images_folder = sys.argv[folders]
+       print(images_folder)
     except IndexError :
         print('Please Make Sure You Have Given Input Folder')
 
     if(os.path.isdir(images_folder)):
+           arr = os.listdir(images_folder)
         
-        arr = os.listdir(images_folder)
-        
-        for items in arr:
-          if items != "site.webmanifest":
-            file, ext = os.path.splitext(items)
-            image = Image.open(images_folder + items)
-            image.thumbnail((600,600), Image.ANTIALIAS)
-
-            image.save(images_folder +file + ext,optimize=True,quality=70)
+           for items in arr:
+                 if items != "site.webmanifest":
+                      file, ext = os.path.splitext(items)
+                      image = Image.open(images_folder + items)
+                      image.thumbnail((600,600), Image.ANTIALIAS)
+                      image.save(images_folder +file + ext,optimize=True,quality=70)
     else:
-        print(f'No Folder {images_folder} ')
+       print(f'No Folder {images_folder} ')
 
 
 
