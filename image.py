aa7eef97b26881b290c7050058b27fa0d2ea212f
Ankit Gupta <ag3831124@gmail.com>
2020-08-07 13:58:44 +0530

    image optimizer

---
+++ image.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from PIL import Image
+
+
+def functionality():
+    
+    try :
+       images_folder = sys.argv[1]
+    
+    except IndexError :
+        print('Please Make Sure You Have Given Input Folder')
+
+    if(os.path.isdir(images_folder)):
+        
+        arr = os.listdir(images_folder)
+        
+        for items in arr:
+            file, ext = os.path.splitext(items)
+            image = Image.open(images_folder + items)
+            image.thumbnail((600,600), Image.ANTIALIAS)
+
+            image.save(images_folder +file + ext,optimize=True,quality=70)
+    else:
+        print(f'No Folder {images_folder} ')
+
+
+
+functionality()
