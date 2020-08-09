aefd1096a8e46178274dd9f6dc7684cf065d1ef9
Ankit Gupta <ag3831124@gmail.com>
2020-08-10 01:10:38 +0530

    some change

--- image_optimizer.py
+++ image_optimizer.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PIL import Image
+from PIL import Image,UnidentifiedImageError
 
 
 def functionality():
@@ -19,10 +19,13 @@ for folders in range(1, len(sys.argv)):
         
            for items in arr:
                  if items != "site.webmanifest":
+                    try :
                       file, ext = os.path.splitext(items)
                       image = Image.open(images_folder + items)
                       image.thumbnail((600,600), Image.ANTIALIAS)
-                      image.save(images_folder +file + ext,optimize=True,quality=70)
+                      image.save(images_folder +file + ext,optimize=True,quality=95)
+                    except UnidentifiedImageError: 
+                      continue
     else:
        print(f'No Folder {images_folder} ')
 
